5c71772daa6328a84e3b797aaa6e106c2a72354e
EstiTw <estitwitowork@gmail.com>
2022-09-18 13:51:11 +0300

    handled submenu links (change links and layout links  dynamically)

--- src/context.js
+++ src/context.js
@@ -7,6 +7,7 @@ export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
   const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: "", links: [] });
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -18,6 +19,8 @@ export const AppProvider = ({ children }) => {
 
   const openSubmenu = (text, coordinates) => {
     setLocation(coordinates);
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
     setIsSubmenuOpen(true);
   };
 
@@ -35,6 +38,7 @@ export const AppProvider = ({ children }) => {
         closeSubmenu,
         closeSidebar,
         location,
+        page,
       }}
     >
       {children}
